938924691c6c61c846bc607a70959b8ea9e56ca6
Paige Niedringhaus <paige_e_niedringhaus@homedepot.com>
2017-08-29 17:12:16 -0400

    progress

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,19 @@
+//ES6 syntax
+import express from 'express';
+import path from 'path';
+import open from 'open';
+
+const port = 3000;
+const app = express();
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, function(err){
+  if(err) {
+    console.log(err);
+  }else{
+    open('http://localhost:' + port);
+  }
+});
