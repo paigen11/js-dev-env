490274b6099b14f26e7ba4dad167e28595360405
Paige Niedringhaus <paige_e_niedringhaus@homedepot.com>
2017-08-31 19:25:04 -0400

    adding more to project skeleton

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -2,9 +2,19 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
+/* eslint-disable no-console */
 
 const port = 3000;
 const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
