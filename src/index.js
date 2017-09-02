a375fe1cca85ca3b9a21e7ea7e643e92defe2e5f
Paige Niedringhaus <paigen11@gmail.com>
2017-09-02 18:26:45 -0400

    adding sample app and mocking data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 import './index.css';
 
-import numeral from 'numeral';
+import {getUsers, deleteUser} from "./api/userApi";
 
-const courseValue = numeral(1000).format('$0,0.00');
-console.log(`I would pay ${courseValue} for this awesome course!`); //eslint-disable-line no-console
+//populate table of users from API call
+getUsers().then(result => {
+  let usersBody = "";
+
+  result.forEach(user => {
+    usersBody+= `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      </tr>`
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  //must use array.from to create a real array from a DOM collection
+  //getElementsByClassName ony returns an 'array like' object
+
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
+});
